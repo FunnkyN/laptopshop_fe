cfc29a957b1f449b35ebd0537009e7510b305621
hoanghoa2212 <hoanghoa221203@gmail.com>
2025-10-26 10:40:55 +0700

    thêm slider ảnh

--- App.js
+++ App.js
@@ -10,25 +10,3 @@ import ChatbotWindow from './Customer/Chatbot/ChatbotWindow';
 import 'katex/dist/katex.min.css';
 import stompService from './Config/stompClient';
 import { receiveMessage } from './Redux/Chat/Action';
-
-// 1. IMPORT COMPONENT MỚI Ở ĐÂY
-import ScrollToTop from './ScrollToTop'; 
-
-function App() {
-  const { auth } = useSelector(store => store);
-  const dispatch = useDispatch();
-  const jwt = localStorage.getItem("jwt");
-  const [isChatbotOpen, setIsChatbotOpen] = useState(false);
-  const location = useLocation();
-
-  const isAdminRoute = location.pathname.startsWith("/admin");
-
-  useEffect(() => {
-    if (jwt) {
-      dispatch(getUser(jwt));
-    }
-  }, [jwt, dispatch]);
-
-  useEffect(() => {
-    if (auth.user && jwt) {
-      console.log("App: Bắt đầu kết nối WebSocket cho user:", auth.user.email);
