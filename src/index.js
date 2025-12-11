6a29af3bd54a8a816da08feb2fef39ad5593d5cf
ngaymaiemdi <tienanh0563@gmail.com>
2025-12-11 11:15:20 +0700

    fix lỗi css

--- src/index.js
+++ src/index.js
@@ -10,8 +10,7 @@ import {store} from './Redux/Store';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <BrowserRouter>
-            <Provider store={store}>
+      store={store}>
                 <App/>
             </Provider>
         </BrowserRouter>
